f38bbeec76b29cf622762797fc23db1a1b35fc09
calistus-igwilo <ebunilo@yahoo.com>
2022-07-15 04:19:33 +0100

    Javascript in browser

--- advanced-javascript.js
+++ advanced-javascript.js
@@ -47,7 +47,7 @@ function logDairy() {
 
 //#############################################
 // Task 1
-function logDairy(arr) {
+/* function logDairy(arr) {
     for (item of arr) {
         console.log(item)
     }
@@ -82,3 +82,151 @@ function animalCan(obj) {
     }
 }
 animalCan(bird);
+ */
+
+
+//#################################################
+// submited to grader with 100% score:
+
+// Task 1
+
+function logDairy() {
+    var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+    for (item of dairy) {
+        console.log(item);
+    }
+}
+logDairy()
+
+// Task 2
+const animal2 = {
+    canJump: true
+};
+
+const bird2 = Object.create(animal2);
+
+bird2.canFly = true;
+
+bird2.hasFeathers = true;
+
+function birdCan() {
+    for (key of Object.keys(bird2)) {
+        console.log(key + ": " + bird2[key]);
+    }
+}
+birdCan();
+
+// Task 3
+function animalCan() {
+    for (prop in bird2) {
+        console.log(prop + ": " + bird2[prop]);
+    }
+}
+animalCan();
+
+
+//###############################################
+// forEach
+const fruits = ['kiwi', 'mango', 'apple', 'pear'];
+
+function appendIndex(fruit, index) {
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+
+
+//###########################################
+// filter
+const nums = [0, 10, 20, 30, 40, 50];
+nums.filter(function(num) {
+    return num > 20;
+})
+
+
+//###########################################
+// map  (allows keys of anytype, the main difference from Objects
+let map = new Map();
+let x = [0, 10, 20, 30, 40, 50].map(function(num) {
+    return num / 10;
+});
+console.log(x);
+// result:  [0, 1, 2, 3, 4, 5]
+
+
+//###########################################
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+};
+const droneKeys = Object.keys(drone);
+droneKeys.forEach(function(key) {
+    result.push(key, drone[key])
+});
+console.log(result);
+// result: ['speed', 100, 'color', 'yellow'
+
+//#########################################
+// Working with Map
+let bestBoxers = new Map();
+bestBoxers.set(1, "The Champion");
+bestBoxers.set(2, "The Runner-up");
+bestBoxers.set(3, "The third place");
+
+console.log(bestBoxers);
+// result: Map(3) {1 => 'The Champion', 2 => 'The Runner-up', 3 => 'The third place'}
+
+bestBoxers.get(1); // 'The Champion'
+
+//###################################
+// set
+const repetitiveFruits = ['apple', 'pear', 'apple', 'pear', 'plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
+// result:  Set(3) { 'apple', 'pear', 'plum' }
+
+
+//#######################################
+// spread
+//
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+console.log(numbersCombined) // [ 1, 2, 3, 4, 5, 6 ]
+
+
+//##############################
+// rest
+
+//Assign the first and second items from numbers to variables and 
+//put the rest in an array:
+const numbers = [1, 2, 3, 4, 5, 6];
+const [one, two, ...rest] = numbers;
+console.log(one); // 1
+console.log(two); // 2
+console.log(rest); // [ 3, 4, 5, 6 ]
+
+
+//#########################################
+// Working with spread and rest
+
+// add objects
+const flying = { wings: 2 }
+const car = { wheels: 4 }
+const flyingCar = {...flying, ...car }
+console.log(flyingCar) // {wings: 2, wheels: 4}
+
+// add new members to an array
+let veggies = ['onion', 'parsley'];
+veggies = [...veggies, 'carrot', 'beetroot'];
+console.log(veggies); // [ 'onion', 'parsley', 'carrot', 'beetroot' ]
+
+// convert a string to an array using spread
+const greeting = "Hello";
+const arrayOfChars = [...greeting];
+console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
+
+//
+const meal = ["soup", "steak", "ice cream"]
+let [starter] = meal;
+console.log(starter); // soup
